d06831107d76c5381ee1ab35f2701d0df19ed8d7
Skyrocket.fund <79742267+Skyrocketfund@users.noreply.github.com>
2023-01-19 16:36:34 +0300

    Add files via upload

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./library/Withdrawable.sol";
+import "./Withdrawable.sol";
 import "./NFTPass_ID.sol";
 
 contract NFT is ERC721Enumerable, Ownable, Withdrawable {
@@ -14,37 +14,71 @@ contract NFT is ERC721Enumerable, Ownable, Withdrawable {
     
     IERC20 public mainToken;
     NFTPassID public nftPassID;
+
+
+    uint256 internal maxTotalSupply_;
     uint256 internal tokenId_;
     string internal baseURI_;
+    string internal fileExtension_;
+
+    uint256[] internal _mintedIds;
 
     mapping(uint256 => uint256) private productPrices;
+    mapping(uint256 => uint256) public nftIDtoProductID;
 
     event SetBaseURI(string newBaseURI);
     event SetMainToken(IERC20 mainToken);
     event Mint(address to, uint256 tokenId);
     event SetProductPrices(uint256[] _productIds, uint256[] _prices);
 
-    constructor(string memory _name, string memory _symbol, string memory _baseUri, IERC20 _mainToken, NFTPassID _nftPassID) ERC721(_name, _symbol) {
+    constructor(
+        uint _maxTotalSupply,
+        string memory _fileExtension,
+        string memory _name,
+        string memory _symbol,
+        string memory _baseUri,
+        IERC20 _mainToken,
+        NFTPassID _nftPassID
+    ) ERC721(_name, _symbol) {
+        maxTotalSupply_ = _maxTotalSupply;
+        fileExtension_ = _fileExtension;
         baseURI_ = _baseUri;
         mainToken = _mainToken;
         nftPassID = _nftPassID;
     }
 
-    function buyNFT(address _to, uint256 _productId, uint256 _price) external {
-        require(nftPassID.balanceOf(msg.sender) == 1, "NFT: You have not nftPassID");
-        require(productPrices[_productId] == _price, "NFT: Incorrect price");
-        mainToken.transferFrom(msg.sender, address(this), _price);
-        _mintTokens(_to, 1);
+    function maxTotalSupply() external view returns(uint) {
+        return maxTotalSupply_;
     }
 
-    function _mintTokens(address _to, uint256 _amountNFT) private {
-        uint256 _lastTokenId = tokenId_;
-        for (uint256 _index = 0; _index < _amountNFT; _index++) {
-            _lastTokenId += 1;
-            _mint(_to, _lastTokenId);
-            emit Mint(_to, _lastTokenId);
-        }
-        tokenId_ += _amountNFT;
+    function setMaxTotalSupply(uint _maxTotalSupply) external onlyOwner {
+        maxTotalSupply_ = _maxTotalSupply;
+    }
+
+    function setFileExtension(string memory newExtension) external onlyOwner {
+        fileExtension_ = newExtension;
+    }
+
+    function buyNFT(address _to, uint256 _productId, uint256 _price) external {
+        require(totalSupply() < maxTotalSupply_ - 1, "NFT: Too many tokens");
+        require(nftPassID.balanceOf(msg.sender) == 1, "NFT: You have not nftPassID");
+        require(productPrices[_productId] == _price, "NFT: Incorrect price");
+
+        mainToken.transferFrom(msg.sender, address(this), _price);
+        _mintTokens(_to, _productId);
+    }
+
+    function _mintTokens(address _to, uint256 _productId) private {
+        tokenId_ += 1;
+        nftIDtoProductID[tokenId_] = _productId;
+
+        _mintedIds.push(tokenId_);
+        _mint(_to, tokenId_);
+        emit Mint(_to, tokenId_);
+    }
+
+    function getMitedIds() external view returns (uint256[] memory) {
+        return _mintedIds;
     }
 
     function getIdNFT(address _owner) public view returns (uint256[] memory) {
@@ -63,8 +97,8 @@ contract NFT is ERC721Enumerable, Ownable, Withdrawable {
                     ? string(
                         abi.encodePacked(
                             baseURI_,
-                            Strings.toString(_tokenId),
-                            ".json"
+                            Strings.toString(nftIDtoProductID[_tokenId]),
+                            fileExtension_
                         )
                     )
                     : "";
@@ -88,7 +122,7 @@ contract NFT is ERC721Enumerable, Ownable, Withdrawable {
 
     function setMainToken(IERC20 _mainToken) external onlyOwner {
         mainToken = _mainToken;
-        emit SetMainToken(_mainToken);
+        //emit SetMainToken(_mainToken);
     }
 
     function setProductPrices(uint256[] memory _productIds, uint256[] memory _prices) external onlyOwner {
